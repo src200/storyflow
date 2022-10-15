d78e9a1108218812d9d0656a70d9016a629a5f26
Challa Sharath <csharath@outlook.com>
2022-10-15 11:00:37 +0530

    streamline configs

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,12 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: [
+    "next",
+    "turbo",
+    "prettier",
+    "plugin:baseui/recommended"
+  ],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
+    '@next/next/no-html-link-for-pages': "off",
+    'react/jsx-key': "off"
   },
 };
