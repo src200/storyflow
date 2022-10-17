bf6565c4362e678f1522bf5d9ee1b9edf26a1175
Challa Sharath <csharath@outlook.com>
2022-10-17 09:59:48 +0530

    fix node-canvas build error

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${process.env.PWD
+    }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
 module.exports = {
   reactStrictMode: false,
   webpack: function (config) {
