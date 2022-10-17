cdc74dd47d62f9df027ffe8592a4752978f8f8dd
Challa Sharath <csharath@outlook.com>
2022-10-17 10:08:02 +0530

    fix node-canvas build error

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -1,13 +1,15 @@
-/** @type {import('next').NextConfig} */
+// For building node-canvas on vercel: https://github.com/Automattic/node-canvas/issues/1779
 if (
   process.env.LD_LIBRARY_PATH == null ||
   !process.env.LD_LIBRARY_PATH.includes(
-    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+    `${process.env.PWD}/node_modules/.pnpm/canvas@2.10.1/node_modules/canvas/build/Release:`,
   )
 ) {
   process.env.LD_LIBRARY_PATH = `${process.env.PWD
-    }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+    }/node_modules/.pnpm/canvas@2.10.1/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
 }
+
+/** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: false,
   webpack: function (config) {
