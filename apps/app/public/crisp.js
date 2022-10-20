3f23a345c6be4dab30abe14963505ca2eda5ec0c
Challa Sharath <csharath@outlook.com>
2022-10-20 23:42:02 +0530

    make crisp chat id public

--- apps/app/public/crisp.js
+++ apps/app/public/crisp.js
@@ -1,6 +1,6 @@
 (function () {
     window.$crisp = [];
-    window.CRISP_WEBSITE_ID = process.env.NEXT_PUBLIC_CRISP_WEBSITE_ID;
+    window.CRISP_WEBSITE_ID = "5669d6a4-b27d-4f02-87c0-cd17fada72d5";
     d = document; s = d.createElement("script");
     s.src = "https://client.crisp.chat/l.js";
     s.async = 1;
